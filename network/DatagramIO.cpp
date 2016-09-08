a57fc67068bdb0c9483222f30654df32ade86efb
Josh Blum <josh@joshknows.com>
2016-09-08 16:44:25 -0700

    working implementation of udp io

--- network/DatagramIO.cpp
+++ network/DatagramIO.cpp
@@ -3,7 +3,9 @@
 
 #include <Pothos/Framework.hpp>
 #include <Poco/URI.h>
+#include <Poco/Logger.h>
 #include <Poco/Net/DatagramSocket.h>
+#include <algorithm> //min/max
 #include <iostream>
 
 /***********************************************************************
@@ -31,7 +33,8 @@
  * |preview disable
  *
  * |param uri[URI] The bind or connection uri string.
- * |default "udt://192.168.10.2:1234"
+ * |default "udp://localhost:1234"
+ * |widget StringEntry()
  *
  * |param opt[Option] Control if the socket is a server (BIND) or client (CONNECT).
  * |option [Connect] "CONNECT"
@@ -65,6 +68,7 @@ public:
     }
 
     DatagramIO(const Pothos::DType &dtype):
+        _logger(Poco::Logger::get("DatagramIO")),
         _packetMode(false),
         _mtu(1472)
     {
@@ -87,13 +91,14 @@ public:
             Poco::URI uriObj(uri);
             const Poco::Net::SocketAddress addr(uriObj.getHost(), uriObj.getPort());
             if (opt == "CONNECT") _sock.connect(addr);
-            else if (opt == "BIND") _sock.bind(addr);
+            else if (opt == "BIND") _sock.bind(addr, true/*reuse*/);
             else throw Pothos::FileException("DatagramIO::setupSocket("+uri+" -> "+opt+")", "unknown option");
         }
         catch (const Poco::Exception &ex)
         {
             throw Pothos::InvalidArgumentException("DatagramIO::setupSocket("+uri+" -> "+opt+")", ex.displayText());
         }
+        _socketConnected = (opt == "CONNECT");
     }
 
     void setMode(const std::string &mode)
@@ -105,18 +110,139 @@ public:
 
     void setMTU(const size_t mtu)
     {
+        auto outPort = this->output(0);
+        const size_t elemSize = outPort->dtype().size();
+        if ((mtu % elemSize) != 0) throw Pothos::InvalidArgumentException("DatagramIO::setMTU("+std::to_string(mtu)+")",
+            "The MTU is not a multiple of the output data-type size: " + outPort->dtype().toString());
+
+        outPort->setReserve(_mtu/elemSize);
         _mtu = mtu;
     }
 
     void work(void)
     {
-        
+        auto inPort = this->input(0);
+        bool hadEvent = false;
+
+        //incoming packet to send
+        if (inPort->hasMessage())
+        {
+            const auto msg = inPort->popMessage();
+            if (msg.type() != typeid(Pothos::Packet))
+            {
+                poco_error_f1(_logger, "Dropped input message of type %s; only Pothos::Packet supported", msg.getTypeString());
+            }
+            const auto &pkt = msg.extract<Pothos::Packet>();
+            this->sendBuffer(pkt.payload);
+            hadEvent = true;
+        }
+
+        //incoming stream to send (clip to MTU)
+        auto inBuff = inPort->buffer();
+        if (inBuff.length != 0)
+        {
+            //clip to the MTU size
+            inBuff.length = std::min(inBuff.length, _mtu);
+
+            //preserve element multiples
+            const size_t elemSize = inBuff.dtype.size();
+            inBuff.length = (inBuff.length/elemSize)*elemSize;
+
+            inPort->consume(inBuff.length);
+            this->sendBuffer(inBuff);
+            hadEvent = true;
+        }
+
+        //small polling sleep if nothing happened and there is nothing to recv
+        if (not hadEvent and _sock.available() == 0)
+        {
+            const auto pollTimeUs = std::min<Poco::Timespan::TimeDiff>(10, this->workInfo().maxTimeoutNs/1000);
+            _sock.poll(Poco::Timespan(pollTimeUs), Poco::Net::Socket::SELECT_READ);
+        }
+
+        //incoming UDP datagram
+        if (_sock.available() != 0)
+        {
+            auto outPort = this->output(0);
+            auto outBuff = outPort->buffer();
+            try
+            {
+                Poco::Net::SocketAddress recvAddr;
+                int ret = _sock.receiveFrom(outBuff.as<void *>(), outBuff.length, recvAddr);
+                if (ret > 0)
+                {
+                    if ((ret % outBuff.dtype.size()) != 0)
+                    {
+                        poco_warning_f2(_logger,
+                            "Received %d bytes is not a multiple of the output size: %s.\n"
+                            "Until the sender is fixed, expect possible truncation of data.",
+                            ret, outBuff.dtype.toString());
+                    }
+
+                    outBuff.length = ret;
+                    if (_packetMode)
+                    {
+                        Pothos::Packet pkt;
+                        pkt.payload = outBuff;
+                        outPort->popElements(outBuff.elements());
+                        outPort->postMessage(pkt);
+                    }
+                    else
+                    {
+                        outPort->produce(outBuff.elements());
+                    }
+
+                    //the new send-to address for bound sockets
+                    if (not _socketConnected) _sendAddr = recvAddr;
+                }
+                else
+                {
+                    poco_error_f2(_logger, "Socket recv %d bytes failed: ret = %d", int(outBuff.length), ret);
+                }
+            }
+            catch (const Poco::Exception &ex)
+            {
+                poco_error_f2(_logger, "Socket recv %d bytes failed: %s", int(outBuff.length), ex.displayText());
+            }
+        }
     }
 
 private:
+
+    void sendBuffer(const Pothos::BufferChunk &buff)
+    {
+        try
+        {
+            int ret = 0;
+            if (_socketConnected) ret = _sock.sendBytes(buff.as<const void *>(), buff.length);
+            else ret = _sock.sendTo(buff.as<const void *>(), buff.length, _sendAddr);
+
+            if (ret != int(buff.length))
+            {
+                poco_error_f2(_logger, "Socket send %d bytes failed: ret = %d", int(buff.length), ret);
+            }
+        }
+        catch (const Poco::Exception &ex)
+        {
+            if (not _socketConnected and _sendAddr == Poco::Net::SocketAddress())
+            {
+                poco_error(_logger, "A bound socket cannot send until it has received!");
+            }
+            else
+            {
+                poco_error_f2(_logger, "Socket send %d bytes failed: %s", int(buff.length), ex.displayText());
+            }
+        }
+    }
+
+    Poco::Logger &_logger;
     Poco::Net::DatagramSocket _sock;
     bool _packetMode;
     size_t _mtu;
+
+    //bound sockets only send to the last received address
+    bool _socketConnected;
+    Poco::Net::SocketAddress _sendAddr;
 };
 
 static Pothos::BlockRegistry registerDatagramIO(
