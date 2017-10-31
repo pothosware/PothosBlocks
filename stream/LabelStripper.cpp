5436e63f2be7e6e1941645605db257950fe25e7a
Josh Blum <josh@joshknows.com>
2017-10-30 22:31:44 -0500

    Added label stripper block to remove labels in a stream

---
+++ stream/LabelStripper.cpp
@@ -0,0 +1,65 @@
+// Copyright (c) 2017-2017 Josh Blum
+// SPDX-License-Identifier: BSL-1.0
+
+#include <Pothos/Framework.hpp>
+
+/***********************************************************************
+ * |PothosDoc Label Stripper
+ *
+ * The label stripper block passively forwards a stream of data
+ * while removing both stream labels and labels on message Packets.
+ *
+ * |category /Stream
+ * |category /Labels
+ * |keywords stream label remove strip
+ *
+ * |factory /blocks/label_stripper()
+ **********************************************************************/
+class LabelStripper : public Pothos::Block
+{
+public:
+    static Block *make(void)
+    {
+        return new LabelStripper();
+    }
+
+    LabelStripper(void)
+    {
+        this->setupInput(0);
+        this->setupOutput(0, "", this->uid()); //unique domain because of buffer forwarding
+    }
+
+    void work(void)
+    {
+        auto inPort = this->input(0);
+        auto outPort = this->output(0);
+
+        while (inPort->hasMessage())
+        {
+            const auto msg = inPort->popMessage();
+            //clear labels for packet types
+            if (msg.type() == typeid(Pothos::Packet))
+            {
+                auto packet = msg.extract<Pothos::Packet>();
+                packet.labels.clear();
+                outPort->postMessage(packet);
+            }
+            //otherwise forward as usual
+            else outPort->postMessage(msg);
+        }
+
+        //forward buffer
+        auto buff = inPort->buffer();
+        if (buff.length == 0) return;
+        inPort->consume(inPort->elements());
+        outPort->postBuffer(buff);
+    }
+
+    void propagateLabels(const Pothos::InputPort *)
+    {
+        //dont!
+    }
+};
+
+static Pothos::BlockRegistry registerLabelStripper(
+    "/blocks/label_stripper", &LabelStripper::make);
