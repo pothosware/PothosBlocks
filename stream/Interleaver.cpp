752401d8eb2918c45b21de29525348b4d3b55696
Josh Blum <josh@joshknows.com>
2021-01-05 17:51:00 -0600

    interleaver - missing include for back insert

--- stream/Interleaver.cpp
+++ stream/Interleaver.cpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <cstring>
 #include <vector>
+#include <iterator>
 
 /***********************************************************************
  * |PothosDoc Interleaver
